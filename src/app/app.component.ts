03ecfb3ba5eecdc37722f901c04338100ef48726
Guilherme Rudiger <guilherme.rudiger@movtech.com.br>
2022-03-01 12:52:09 -0300

    Finalziação funcionalidades.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,19 +7,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
 
-
-  private PCT_VAL_BAU: number = 35;
-  private PCT_VAL_CLI: number = 35;
-  private PCT_VAL_MEC: number = 30;
-  private PCT_EXT_PARCEIRO: number = 5;
-  private PCT_EXT_BOOSTER: number = 5;
-  private PCT_EXT_ALIANCA: number = 5;
+  private PCT_VAL_BAU: number = 0;
+  private PCT_VAL_CLI: number = 0;
+  private PCT_VAL_MEC: number = 0;
+  private PCT_EXT_PARCEIRO: number = 0;
+  private PCT_EXT_BOOSTER: number = 0;
+  private PCT_EXT_ALIANCA: number = 0;
   private VAL_TOTAL: number = 0;
 
   constructor() { }
 
   calcForm = new FormGroup({
-    Passaporte: new FormControl('Passaporte'),
+    Passaporte: new FormControl('Passaporte',Validators.required),
     ValorTotal: new FormControl('ValorTotal'),
     Parceria: new FormControl('Parceria'),
     Booster: new FormControl('Booster'),
@@ -33,6 +32,12 @@ export class AppComponent implements OnInit{
   }
 
   CalcularValores(){
+    this.PCT_VAL_BAU = 35
+    this.PCT_VAL_CLI = 35
+    this.PCT_VAL_MEC = 30
+    this.PCT_EXT_PARCEIRO = 5
+    this.PCT_EXT_BOOSTER = 5
+    this.PCT_EXT_ALIANCA = 15
     this.VAL_TOTAL = this.calcForm.controls['ValorTotal'].value
     var parceria = this.calcForm.controls['Parceria'].value
     var booster = this.calcForm.controls['Booster'].value
@@ -61,6 +66,39 @@ export class AppComponent implements OnInit{
             this.PCT_VAL_BAU = 0
             this.PCT_VAL_MEC = 0
             this.PCT_VAL_CLI = 0
+
+            var Texto = ''
+
+            if (booster)
+            {
+              Texto = '**Nº:**'
+              Texto +='\n**Passaporte:**' + this.calcForm.controls['Passaporte'].value
+              Texto +='\n**Valor Baú:**' + this.calcForm.controls['ValorBau'].value
+              Texto +='\n**Valor Cliente:**' + this.calcForm.controls['ValorCliente'].value
+              Texto +='\n***Valor Mecânico:***' + this.calcForm.controls['ValorMecanico'].value
+              Texto += '\n**Valor Total:**'  + this.calcForm.controls['ValorTotal'].value
+            }
+            else
+            {
+              Texto = '**Nº:**'
+              Texto +='\n**Passaporte:**' + this.calcForm.controls['Passaporte'].value
+              Texto +='\n**Valor Baú:**' + this.calcForm.controls['ValorBau'].value
+              Texto +='\n**Valor Cliente:**' + this.calcForm.controls['ValorCliente'].value
+              Texto +='\n**Valor Mecânico:**' + this.calcForm.controls['ValorMecanico'].value
+              Texto += '\n**Valor Total:**'  + this.calcForm.controls['ValorTotal'].value
+            }
+
+            const selBox = document.createElement('textarea');
+            selBox.style.position = 'fixed';
+            selBox.style.left = '0';
+            selBox.style.top = '0';
+            selBox.style.opacity = '0';
+            selBox.value = Texto;
+            document.body.appendChild(selBox);
+            selBox.focus();
+            selBox.select();
+            document.execCommand('copy');
+            document.body.removeChild(selBox);
   }
 
   limparCampos(){
@@ -73,7 +111,7 @@ export class AppComponent implements OnInit{
     this.calcForm.controls['Booster'].setValue(false);
     this.calcForm.controls['Passaporte'].setValue('');
     this.calcForm.controls['ValorTotal'].setValue('');
-    this.calcForm.controls['Parceria'].setValue('Sem Parceria');
+    this.calcForm.controls['Parceria'].setValue(0);
   }
 
 }
