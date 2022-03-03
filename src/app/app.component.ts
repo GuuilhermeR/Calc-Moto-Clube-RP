d31f62f154bace089dfc16aecfb3b7fe0ae6ab10
Guilherme Rudiger <guilherme.rudiger@movtech.com.br>
2022-03-03 13:01:06 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
